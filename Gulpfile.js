f7cde608e8d63e76deb5aa171721afb154188f9d
Kristian Andersen <hello@kristian.co>
2016-02-02 21:56:07 +0100

    Use postcss for autoprefixer + minification. Added html minification

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,37 @@
 var gulp = require('gulp');
 var sequence = require('gulp-sequence');
-
 var shell = require('gulp-shell');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var csswring = require('csswring');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('css', function(cb) {
+	var processors = [
+		autoprefixer({browsers: ['last 4 versions', '> 5%']}),
+		csswring
+	];
+	return gulp.src('_site/css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('_site/css'));
+});
+
+gulp.task('htmlmin', function(cb) {
+	return gulp.src('_site/index.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('_site/'))
+});
 
 gulp.task('watch', function(cb) {
-	gulp.src('.', {read: false})
+	return gulp.src('.', {read: false})
 		.pipe(shell([
 			'jekyll serve'
 		]),
 		{verbose: true});
 });
 
-gulp.task('build', function(cb) {
-	gulp.src('.', {read: false})
+gulp.task('jekyll-build', function(cb) {
+	return gulp.src('.', {read: false})
 		.pipe(shell([
 			'jekyll build'
 		]),
@@ -20,15 +39,19 @@ gulp.task('build', function(cb) {
 });
 
 gulp.task('upload', function(cb) {
-	gulp.src('.', {read: false})
+	return gulp.src('.', {read: false})
 		.pipe(shell([
 			's3_website push'
 		]),
 		{verbose: true});
 });
 
+gulp.task('build', function(cb) {
+	return sequence('jekyll-build', ['css', 'htmlmin'], cb);
+});
+
 gulp.task('deploy', function(cb) {
-	sequence(['build', 'upload'], cb);
+	return sequence('build', ['upload'], cb);
 });
 
 gulp.task('default', ['watch'])
